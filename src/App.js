52af57b414a6902bc7d568ea5218e7673db7e2ec
runnerdave <runnerdave@gmail.com>
2017-10-19 21:58:09 +1100

    added state to buttons to display down time

--- src/App.js
+++ src/App.js
@@ -1,59 +1,97 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const buttons = [
+                  ["OBDX SIT1", "OBDX SIT4"],
+                  ["DCO SIT1", "DCO SIT4"],
+                  ["DOCTA SIT1", "DOCTA SIT4"]
+                ];
+
 const Footer = () => {
-  return (
-    <header className="App-footer">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Powered by React</h1>
-    </header>
-  )
+    return (
+        <header className="app-footer">
+            <img src={logo} className="app-logo" alt="logo"/>
+            <h1 className="app-title">Powered by React</h1>
+        </header>
+    )
 }
 
-const Button = (props) => {
-  return (
-    <button type="button" className={`btn btn-${props.status} btn-block`}>{props.text}</button>
-  )
+
+
+class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isUp: true,
+            text: props.text
+        }
+    }
+
+    timer() {
+      const timeDisplay = new Date().toLocaleTimeString();
+
+      return (
+          ` has been down since: ${timeDisplay}`
+      )
+    }
+
+    handleClick() {
+        let buttonText = this.state.text;
+        if (this.state.isUp) {
+            buttonText = this.props.text + this.timer();
+        } else {
+            buttonText = this.props.text;
+        }
+        this.setState({
+            isUp: !this.state.isUp,
+            text: buttonText
+        })
+    }
+
+    render() {
+        return <button className={`btn btn-${this.state.isUp ? 'success' : 'danger'} btn-block`}
+                       onClick={() => this.handleClick()}>{this.state.text}</button>
+    }
 }
 
 const Row = (props) => {
-  return (
-    <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-3"><Button text={props.buttons[0]} status="success" /></div>
-      <div className="col-md-2"></div>
-      <div className="col-md-3"><Button text={props.buttons[1]} status="danger" /></div>
-      <div className="col-md-2"></div>
-    </div>
-  )
+    return (
+        <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-3"><Button text={props.buttons[0]}/></div>
+            <div className="col-md-2"></div>
+            <div className="col-md-3"><Button text={props.buttons[1]}/></div>
+            <div className="col-md-2"></div>
+        </div>
+    )
 }
 
 const SpacerRow = () => {
-  return (
-    <div className="row">
-      <div className="col-md-12 Row-spacer"></div>
-    </div>
-  )
+    return (
+        <div className="row">
+            <div className="col-md-12 row-spacer"></div>
+        </div>
+    )
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">        
-        <div className="jumbotron" style={{height: '100vh'}}>
-          <div className="container-fluid">
-            <Row buttons={["OBDX SIT1", "OBDX SIT4"]} />
-            <SpacerRow />
-            <Row buttons={["DCO SIT1", "DCO SIT4"]} />
-            <SpacerRow />
-            <Row buttons={["DOCTA SIT1", "DOCTA SIT4"]} />
-          </div>
-        </div>
-        <Footer />
-      </div>      
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <div className="jumbotron" style={{height: '100vh'}}>
+                    <div className="container-fluid">
+                        <Row buttons={buttons[0]}/>
+                        <SpacerRow/>
+                        <Row buttons={buttons[1]}/>
+                        <SpacerRow/>
+                        <Row buttons={buttons[2]}/>
+                    </div>
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
